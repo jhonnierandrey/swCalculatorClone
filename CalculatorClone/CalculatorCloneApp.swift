4cfdd98971e7976e59d1a5df5b15fde6b9337532
jhonnierandrey <jhonnierandrey@outlook.com>
2023-06-28 11:29:01 -0300

    initial commit

---
+++ CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by Jhonnier Zapata on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CalculatorClone
+//
+//  Created by Jhonnier Zapata on 6/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
